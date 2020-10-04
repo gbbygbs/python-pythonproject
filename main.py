27d5719e11984bab90dab3a9560f4eafd57bbf24
Gab <gbbygbs10@gmail.com>
2020-10-04 23:10:53 +0800

    New branch

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 def main(msg):
-<<<<<<< HEAD
-    # Adding a second comment to this file.
-    # Print a message
-=======
-    // Print a message
->>>>>>> c58e52c33454cefb4aeb5d36a9e43d43ae332a70
+    # No longer need comments here
     print(msg)
+
+def secondary():
+    print("New function")
 
 main("Hello People")
